286473ed42159cfde497fa84a4e96c5cff0c8259
ehanson8 <ehanson8@jh.edu>
2017-04-07 15:46:11 -0400

    script updates

--- viafReconciliationPeople.py
+++ viafReconciliationPeople.py
@@ -1,22 +1,22 @@
 import requests
 import csv
-from bs4 import BeautifulSoup
 from fuzzywuzzy import fuzz
 import json
 import urllib
 
 baseURL = 'http://viaf.org/viaf/search/viaf?query=local.personalNames+%3D+%22'
 f=csv.writer(open('viafPeopleResults.csv', 'wb'))
-f.writerow(['search']+['result']+['ratio']+['partialRatio']+['tokenSort']+['tokenSet']+['avg']+['viaf']+['lc']+['isni'])
+f.writerow(['search']+['result']+['viaf']+['lc']+['isni']+['ratio']+['partialRatio']+['tokenSort']+['tokenSet']+['avg'])
 with open('people.txt') as txt:
     for row in txt:
         rowEdited = urllib.quote(row.decode('utf-8-sig').encode('utf-8').strip())
-        url = baseURL+rowEdited+'%22+and+local.sources+%3D+%22lc%22&sortKeys=holdingscount&httpAccept=application/rdf+xml'
+        url = baseURL+rowEdited+'%22+and+local.sources+%3D+%22lc%22&sortKeys=holdingscount&maximumRecords=1&httpAccept=application/rdf+json'
         response = requests.get(url).content
-        record = BeautifulSoup(response, "lxml").find('html').find('body').find('searchretrieveresponse')
+        response = response[response.index('<recordData xsi:type="ns1:stringOrXmlFragment">')+47:response.index('</recordData>')].replace('&quot;','"')
+        response = json.loads(response)
         try:
-            label = record.find('records').find('record').find( 'recorddata').find('ns2:viafcluster').find('ns2:mainheadings').find('ns2:data').find('ns2:text').text.encode('utf-8')
-            viafid = record.find('records').find('record').find( 'recorddata').find('ns2:viafcluster').find('ns2:viafid').text.encode('utf-8')
+            label = response['mainHeadings']['data'][0]['text']
+            viafid = response['viafID']
         except:
             label = ''
             viafid = ''
@@ -40,4 +40,4 @@ with open('people.txt') as txt:
             lc = ''
             isni = ''
         f=csv.writer(open('viafPeopleResults.csv', 'a'))
-        f.writerow([row.strip()]+[label]+[ratio]+[partialRatio]+[tokenSort]+[tokenSet]+[avg]+[viafid]+[lc]+[isni])
+        f.writerow([row.strip()]+[label]+[viafid]+[lc]+[isni]+[ratio]+[partialRatio]+[tokenSort]+[tokenSet]+[avg])
