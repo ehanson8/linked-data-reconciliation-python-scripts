10a44c83c23d23dd7c892ad1168a7c030791d4bb
ehanson8 <ehanson8@jh.edu>
2017-04-17 13:09:31 -0400

    updates

--- dbpediaReconciliationGeneral.py
+++ dbpediaReconciliationGeneral.py
@@ -5,27 +5,30 @@ from fuzzywuzzy import fuzz
 import urllib
 
 baseURL = 'http://lookup.dbpedia.org/api/search/KeywordSearch?MaxHits=1&QueryString='
-typeQualifier = '&QueryClass=organisation'#Use DBpedia classes listed here (http://mappings.dbpedia.org/server/ontology/classes/) to refine results provided that all of the entities in the source file are of the same types
-f=csv.writer(open('dbpediaResultsCorporate.csv', 'wb'))
-f.writerow(['search']+['result']+['ratio']+['partialRatio']+['tokenSort']+['tokenSet']+['avg']+['uri'])
+
+
+f=csv.writer(open('dbpediaResultsGeneral.csv', 'wb'))
+f.writerow(['search']+['result']+['description']+['ratio']+['partialRatio']+['tokenSort']+['tokenSet']+['avg']+['uri'])
 with open('organizations.csv') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
         name = str(row['name'])
         nameEdited = urllib.quote(name.decode('utf-8-sig').encode('utf-8').strip())
-        url = baseURL+nameEdited.strip()+typeQualifier
+        url = baseURL+nameEdited.strip()
         response = requests.get(url).content
         record = BeautifulSoup(response, "lxml").find('html').find('body').find('arrayofresult').find('result')
         try:
             label = record.find('label').text.encode('utf-8')
             uri = record.find('uri').text.encode('utf-8')
+            description = record.find('description').text.encode('utf-8')
         except:
             label = ''
             uri = ''
+            description = ''
         ratio = fuzz.ratio(name, label)
         partialRatio = fuzz.partial_ratio(name, label)
         tokenSort = fuzz.token_sort_ratio(name, label)
         tokenSet = fuzz.token_set_ratio(name, label)
         avg = (ratio+partialRatio+tokenSort+tokenSet)/4
-        f=csv.writer(open('dbpediaResultsCorporate.csv', 'a'))
-        f.writerow([name.strip()]+[label]+[ratio]+[partialRatio]+[tokenSort]+[tokenSet]+[avg]+[uri])
+        f=csv.writer(open('dbpediaResultsGeneral.csv', 'a'))
+        f.writerow([name.strip()]+[label]+[description.strip()]+[ratio]+[partialRatio]+[tokenSort]+[tokenSet]+[avg]+[uri])
